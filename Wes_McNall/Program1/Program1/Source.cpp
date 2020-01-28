14192ab70875dd1eb9fae347ff274f36c69c4525
mwmcnall <mwmcnall@gmail.com>
2020-01-28 17:15:22 -0600

    Program 1 Finished

--- Wes_McNall/Program1/Program1/Source.cpp
+++ Wes_McNall/Program1/Program1/Source.cpp
@@ -7,13 +7,85 @@
 #include <cstdlib>
 #include <ctime>
 #include <cctype>
+#include <string>
 using namespace std;
+
+void Output_Score(int you_score, int ai_score);
+int Player_Turn(int pot);
+int AI_Turn(int pot);
+bool Play_Again();
+
+int main() {
+
+	int pot = 0, you_score, ai_score;
+	char input;
+	srand(time(0));
+
+	// Program Loop
+	while (1 == 1) {
+		
+		you_score = 0;
+		ai_score = 0;
+		Output_Score(0, 0);
+		// Game Loop
+		while (ai_score <= 50) {
+			pot = Player_Turn(0);
+			you_score += pot;
+			if (you_score >= 50) {
+				break;
+			}
+			Output_Score(you_score, ai_score);
+			pot = AI_Turn(0);
+			ai_score += pot;
+			Output_Score(you_score, ai_score);
+		}
+
+		Output_Score(you_score, ai_score);
+		if (you_score > ai_score) {
+			cout << "Humans win!";
+		}
+		else {
+			cout << "Machines win!";
+		}
+
+		if (Play_Again()) {
+			continue;
+		}
+		else {
+			break;
+		}
+	}
+
+	cout << "Thanks for playing!" << endl;
+
+	return 0;
+
+}
+
+int _Roll_And_Output(int die, int pot) {
+	die = Roll_Die();
+	if (_Dice_Bust(die)) {
+		cout << "Dice Roll 1: BUST" << endl << endl;
+		return -1;
+	}
+	Output_Roll_Pot(die, (die + pot));
+	return die;
+}
 
 bool _Dice_Bust(int die) {
 	if (die == 1) {
 		return true;
 	}
 	return false;
+}
+
+char _Player_input() {
+	// Might fail if player inputs something not char?
+	char input;
+
+	cout << "\t(R)oll again or (H)old? ";
+	cin >> input;
+	return toupper(input);
 }
 
 int Roll_Die() {
@@ -27,43 +99,11 @@ void Output_Roll_Pot(int die, int pot) {
 	return;
 }
 
-void Hold() {
-	return;
-}
-
-void Increase_Player_Score() {
-	return;
-}
-
-void Reset_Pot() {
-	return;
-}
-
 void Output_Score(int you_score, int ai_score) {
 
 	cout << "Score\t" << "You : " << you_score << " AI : " << ai_score << endl << endl;
 
 	return;
-}
-
-int _Roll_And_Output(int die, int pot) {
-	die = Roll_Die();
-	if (_Dice_Bust(die)) {
-		cout << "Dice Roll 1: BUST" << endl << endl;
-		return -1;
-	}
-	//pot += die;
-	Output_Roll_Pot(die, (die + pot));
-	return die;
-}
-
-char _Player_input() {
-	// Might fail if player inputs something not char?
-	char input;
-
-	cout << "\t(R)oll again or (H)old? ";
-	cin >> input;
-	return toupper(input);
 }
 
 int Roll(int die, int pot) {
@@ -79,10 +119,22 @@ int Roll(int die, int pot) {
 	}
 }
 
-int Player_Turn(int die, int pot) {
+void Turn_Announce(string team) {
+	char c;
+	cout << team << " Turn... " << "Hit enter to continue.";
+	cin.ignore();
+	// For some reason if I didn't call this it would skip hitting enter for AI
+	if (team == "AI") {
+		cin.ignore();
+	}
+	return;
+}
+
+int Player_Turn(int pot) {
+	int die = 0;
 	char input;
 
-	cout << "Players Turn" << endl;
+	Turn_Announce("Player");
 
 	pot = Roll(die, pot);
 	if (pot == 0) { // Bust check
@@ -110,9 +162,11 @@ int Player_Turn(int die, int pot) {
 	}
 }
 
-int AI_Turn(int die, int pot) {
+int AI_Turn(int pot) {
 
-	cout << "AI Turn" << endl;
+	int die = 0;
+
+	Turn_Announce("AI");
 
 	do {
 		pot = Roll(die, pot);
@@ -150,58 +204,4 @@ bool Play_Again() {
 		return true;
 	}
 	return false;
-}
-
-int main() {
-
-	int die=0, pot = 0, you_score, ai_score;
-	char input;
-	srand(time(0));
-
-	// Program Loop
-	while (1 == 1) {
-		// Game Loop
-		you_score = 0;
-		ai_score = 0;
-		Output_Score(0, 0);
-		while (ai_score <= 50) {
-			pot = Player_Turn(die, 0);
-			you_score += pot;
-			if (you_score >= 50) {
-				break;
-			}
-			Output_Score(you_score, ai_score);
-			pot = AI_Turn(die, 0);
-			ai_score += pot;
-			Output_Score(you_score, ai_score);
-		}
-
-		Output_Score(you_score, ai_score);
-		if (you_score > ai_score) {
-			cout << "Humans win!";
-		}
-		else {
-			cout << "Machines win!";
-		}
-
-		if (Play_Again()) {
-			continue;
-		}
-		else {
-			break;
-		}
-	}
-
-	// Game stops when someone's score is >= 50
-
-	// Automatic dice roll once
-	// Check for input 'r' or 'h'
-	// Sanitize input
-	// Go on until hold
-	// Output score
-	// AI turn
-	// Repeat player turn
-
-	return 0;
-
 }
